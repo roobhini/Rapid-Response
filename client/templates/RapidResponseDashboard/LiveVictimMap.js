cea862c83f633de7f47db939e9a738ce1ea089ce
aditya menon <hello@adityamenon.co>
2015-11-29 08:23:44 +0530

    most functionality on dashboard maps working

--- client/templates/RapidResponseDashboard/LiveVictimMap.js
+++ client/templates/RapidResponseDashboard/LiveVictimMap.js
@@ -24,48 +24,70 @@ Template.LiveVictimMap.helpers({
 
 Template.LiveVictimMap.onCreated(function() {
   GoogleMaps.ready('liveVictimMap', function(map) {
-    let DistressSignalBubbleHandler = function (DistressSignal, markers) {
+    var info_window = new google.maps.InfoWindow({content: ""}),
+        markers = {},
+        DistressQuery = DistressSignals.find({"helped": false});
+
+    DistressQuery.fetch().forEach((DistressSignal) => {
       // Create a marker for this document
       var marker = new google.maps.Marker({
         draggable: false,
         animation: google.maps.Animation.DROP,
-        position: new google.maps.LatLng(document.latitudes, document.Longitudes),
+        position: new google.maps.LatLng(DistressSignal.coords.lat, DistressSignal.coords.lng),
         map: map.instance,
-        clickable:true,
+        clickable: true,
         id: DistressSignal._id
       });
 
       google.maps.event.addListener(marker, 'click', function(marker) {
+        console.log(this)
         info_window.setContent(Blaze.toHTMLWithData(Template.DistressMapBubble, document));
         info_window.open(this.getMap(), this);
 
         $(".helpedBtn").click(function () {
-          DistressSignals.update(marker.id, {'$set': {'helped': true}});
+          Meteor.call('finishedHelpingDistressSignal', DistressSignal);
         });
       });
 
       // Store this marker instance within the markers object.
       markers[marker.id] = marker;
-    };
-
-    var info_window = new google.maps.InfoWindow({content: ""}),
-        markers = {},
-        DistressQuery = DistressSignals.find({"helped": false});
-
-    DistressQuery.fetch().forEach((DistressSignal) => {DistressSignalBubbleHandler(DistressSignal, markers)});
+    });
 
     DistressQuery.observe({
-      added: (observedDoc) => {DistressSignalBubbleHandler(observedDoc)},
-      removed: function(oldDocument) {
+      added: (DistressSignal) => {
+        // Create a marker for this document
+        var marker = new google.maps.Marker({
+          draggable: false,
+          animation: google.maps.Animation.DROP,
+          position: new google.maps.LatLng(DistressSignal.coords.lat, DistressSignal.coords.lng),
+          map: map.instance,
+          clickable: true,
+          id: DistressSignal._id
+        });
+
+        google.maps.event.addListener(marker, 'click', function(marker) {
+          console.log(this)
+          info_window.setContent(Blaze.toHTMLWithData(Template.DistressMapBubble, document));
+          info_window.open(this.getMap(), this);
+
+          $(".helpedBtn").click(function () {
+            Meteor.call('finishedHelpingDistressSignal', DistressSignal);
+          });
+        });
+
+        // Store this marker instance within the markers object.
+        markers[marker.id] = marker;
+      },
+      removed: (observedDoc) => {
         // Remove the marker from the map
-        markers[oldDocument._id].setMap(null);
+        markers[observedDoc._id].setMap(null);
 
         // Clear the event listener
         google.maps.event.clearInstanceListeners(
-          markers[oldDocument._id]);
+          markers[observedDoc._id]);
 
         // Remove the reference to this marker instance
-        delete markers[oldDocument._id];
+        delete markers[observedDoc._id];
       }
     });
   });
--- server/methods.js
+++ server/methods.js
@@ -3,5 +3,8 @@ Meteor.methods({
     var wat = DistressSignals.insert(distress);
     console.log(wat);
     return wat;
+  },
+  'finishedHelpingDistressSignal'(distressSignal) {
+    DistressSignals.update(distressSignal._id, {'$set': {'helped': true}});
   }
 });
